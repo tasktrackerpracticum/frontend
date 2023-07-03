f1ff8d64579f7f4d364cf5387fd0c8b60272d034
BJuice <beetle_juice84@mail.ru>
2023-07-03 21:14:50 +0300

    feat/Добавить настройки Prettier, Linter, Husky, SASS

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,19 +1,19 @@
 class MainApi {
-    constructor({baseUrl, headers}) {
-      this._baseUrl = baseUrl;
-      this._headers = headers
-    }
-  
-    _handleResponse(res) {
-        return (res.ok) ? (res.json()) : (Promise.reject(`Ошибка: ${res.status}`))
-    }
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  _handleResponse(res) {
+    return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
+  }
 }
-  
+
 const mainApi = new MainApi({
-    baseUrl: '',
-    headers: {
-      'Content-Type': 'application/json'
-    }
+  baseUrl: "",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-  
+
 export default mainApi;
