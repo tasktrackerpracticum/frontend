bf0bafeb158b02980c929e560eae38a6867118f0
BJuice <beetle_juice84@mail.ru>
2023-07-04 09:03:21 +0300

    feat/Создаёт первую структуру. Добавляет компоненты App, Header + стили SCSS

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../Header/Header";
+import '../../scss/app.scss';
+
+
+function App() {
+
+  return(
+    <div className="page">
+      <div className="page__container">
+        <Header />
+
+      </div>
+    </div>
+  )
+}
+
+export default App;
