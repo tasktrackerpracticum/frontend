6c6f535df45adaac8d9b8463143904c607df0216
Kirill Karpov <rin.veber@gmail.com>
2023-07-11 22:35:09 +0400

    удаление menu и sideNavigation

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,9 @@
 import React, { useState } from "react";
 import Header from "../Header/Header.jsx";
 import "../../scss/app.scss";
-import SideNavigation from "../SideNavigation/SideNavigation.jsx";
-import Menu from "../Menu/Menu.jsx";
 import Profile from "../Profile/Profile.jsx"
 
 function App() {
-	const [menuActive, setMenuActive] = useState(false);
   const [profileActive, setProfileActive] = useState(false);
 
 	return (
@@ -14,8 +11,6 @@ function App() {
 			<div className="page">
 				<div className="page__container">
 					<Header active={profileActive} setActive={setProfileActive}/>
-					<Menu menuActive={menuActive} setMenuActive={setMenuActive} />
-					<SideNavigation active={menuActive} setActive={setMenuActive} />
 					<Profile active={profileActive} setActive={setProfileActive} />
 				</div>
 			</div>
