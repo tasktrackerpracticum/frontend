bf996a547a4015b28099de509a3200aa2ec47382
Valentina Lugovaia <vaal.it@yandex.ru>
2023-07-11 17:05:13 +0300

    message

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,43 @@
-import React, { useState } from "react";
-import Header from "../Header/Header.jsx";
-import "../../scss/components/app.scss";
-import SideNavigation from "../SideNavigation/SideNavigation.jsx";
-import Menu from "../Menu/Menu.jsx";
+import { useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import '../../scss/components/app.scss';
+import useAuth from '../../hooks/useAuth';
+import Header from '../Header/Header.jsx';
+import SideNavigation from '../SideNavigation/SideNavigation.jsx';
+import Menu from '../Menu/Menu.jsx';
+import Register from '../Register/Register.jsx';
+import Login from '../Login/Login.jsx';
+import NotFoundPage from '../NotFoundPage/NotFoundPage.jsx';
 
 function App() {
-	const [menuActive, setMenuActive] = useState(false);
+  const [menuActive, setMenuActive] = useState(false);
 
-	return (
-		<>
-			<div className="page">
-				<div className="page__container">
-					<Header />
-					<Menu menuActive={menuActive} setMenuActive={setMenuActive} />
-					<SideNavigation active={menuActive} setActive={setMenuActive} />
-				</div>
-			</div>
-		</>
-	);
+  const { handleLogin, handleRegister } = useAuth();
+
+  return (
+    <>
+      <div className='page'>
+        <div className='page__container'>
+          <Switch>
+            <Route exact path='/'>
+              <Header />
+              <Menu menuActive={menuActive} setMenuActive={setMenuActive} />
+              <SideNavigation active={menuActive} setActive={setMenuActive} />
+            </Route>
+            <Route path='/register' onRegister={handleRegister}>
+              <Register />
+            </Route>
+            <Route path='/login' onLogin={handleLogin}>
+              <Login />
+            </Route>
+            <Route path='*'>
+              <NotFoundPage />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
