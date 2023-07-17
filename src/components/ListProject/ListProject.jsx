b57aff11ac8c77f0d0229681a75eb587bf7d1eff
Kirill Karpov <rin.veber@gmail.com>
2023-07-17 11:40:51 +0400

    Чистка кода

--- src/components/ListProject/ListProject.jsx
+++ src/components/ListProject/ListProject.jsx
@@ -4,12 +4,11 @@ import { useEffect } from 'react';
 import { fetchProjects } from '../../services/projectsSlice';
 
 export default function ListProject({ isOpen, setOpen }) {
-
   const dispatch = useDispatch();
-  const { status, error, projects } = useSelector(state => state.projects);
+  const { status, error, projects } = useSelector((state) => state.projects);
 
   useEffect(() => {
-    dispatch(fetchProjects())
+    dispatch(fetchProjects());
   }, [dispatch]);
 
   const openTaskCreate = () => {
@@ -24,36 +23,36 @@ export default function ListProject({ isOpen, setOpen }) {
           onClick={openTaskCreate}
         ></button>
       </div>
-        {status === 'loading' && <h2>loading...</h2>}   {/* потом добавить спиннер и убрать */}
-        {error && <h2>{error}</h2>}                     {/* потом добавить модалку ошибки и убрать */}
+      {status === 'loading' && <h2>loading...</h2>}{' '}
+      {/* потом добавить спиннер и убрать */}
+      {error && <h2>{error}</h2>} {/* потом добавить модалку ошибки и убрать */}
       <div className='listProject__content'>
+        {/* потом добавить компонент вместо вставки разметки. Либо сделать на роуте со списком проектов с модалкой справа, чтобы не запрашивать список проектов */}
 
-{/* потом добавить компонент вместо вставки разметки. Либо сделать на роуте со списком проектов с модалкой справа, чтобы не запрашивать список проектов */}
-
-        {projects.length !== 0 && (projects.map((item) => {
-              return (
-                <div key={item.id} className='listProject__container'>
-                  <div className='listProject__project-title'>{item.title}</div>
-                  <div className='listProject__project-info'>
-                  <div className='listProject__project-time'>16.06.2023-16.06.2023</div>
+        {projects.length !== 0 &&
+          projects.map((item) => {
+            return (
+              <div key={item.id} className='listProject__container'>
+                <div className='listProject__project-title'>{item.title}</div>
+                <div className='listProject__project-info'>
+                  <div className='listProject__project-time'>
+                    16.06.2023-16.06.2023
+                  </div>
                   <div className='listProject__wrap'>
-                  <div className='listProject__project-member'>...</div>
+                    <div className='listProject__project-member'>...</div>
 
-                  <div className='listProject__project-status'>
-                  Активен
+                    <div className='listProject__project-status'>Активен</div>
                   </div>
                 </div>
               </div>
-            </div>
-
-              )
-            }))}
+            );
+          })}
       </div>
     </section>
   );
 }
 
 ListProject.propTypes = {
-    isOpen: openFunctionType,
-    setOpen: openFunctionType,
-  };
+  isOpen: openFunctionType,
+  setOpen: openFunctionType,
+};
