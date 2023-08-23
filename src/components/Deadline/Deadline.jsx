91118a8bbaeee07e3964067ec45c967807cc3361
BJuice <beetle_juice84@mail.ru>
2023-08-23 21:49:43 +0300

    fix/Отрефакторить CSS в компонентах SCSS: deadline, task, taskContainer, projectList, postProject

--- src/components/Deadline/Deadline.jsx
+++ src/components/Deadline/Deadline.jsx
@@ -2,9 +2,9 @@ import { stringType } from '../../constatnts/prop-types';
 
 export default function Deadline({ start, finish }) {
   return (
-    <section className='deadline'>
+    <span className='deadline'>
       {start} - {finish}
-    </section>
+    </span>
   );
 }
 
