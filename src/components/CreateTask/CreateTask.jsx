5a2c22e828bfe286d55e3a0d5c0d1420e889ce29
BJuice <beetle_juice84@mail.ru>
2023-08-26 15:19:22 +0300

    fix/Переиспользовать InputField в CreateTask

--- src/components/CreateTask/CreateTask.jsx
+++ src/components/CreateTask/CreateTask.jsx
@@ -1,140 +1,66 @@
-import { useState } from 'react';
 import { boolType, functionType } from '../../constatnts/prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { createNewTask } from '../../services/tasksSlice';
-import avatar from '../../images/user-avatar-profile.png';
-import Select from '../Select/Select';
 import { useLocation } from 'react-router-dom';
+import { FormProvider, useForm } from 'react-hook-form';
+import InputField from '../CreateProject/InputField';
 
 function CreateTask({ active, setActive }) {
-  const [isActiveListPerformer, setActliveListPerformer] = useState(false);
-  const [title, setTitle] = useState();
-  const [deadline, setDeadline] = useState('');
-  const [disabled, setDisabled] = useState(true);
-
-  const todayDate = new Date(); 
-  const formatDate = todayDate.getDate() < 10 ? `0${todayDate.getDate()}`:todayDate.getDate();
-  const formatMonth = todayDate.getMonth() < 10 ? `0${todayDate.getMonth()}`: todayDate.getMonth();
-  const formattedDate = [todayDate.getFullYear(), formatMonth, formatDate].join('-');
-  const currentDate = formattedDate;
+  const location = useLocation();
+  const projectId = parseInt(location.pathname.match(/\d+/));
 
   const currentUser = useSelector((state) => state.user.user);
   const dispatch = useDispatch();
 
-  const location = useLocation();
-  const projectId = parseInt(location.pathname.match(/\d+/));
+  console.log(currentUser)
 
-  const handleAction = (e) => {
+  const methods = useForm({
+    defaultValues: {
+      title: '',
+      description: '',
+      date_start: null,
+      date_finish: null,
+      users: [],
+    },
+  });
+
+
+
+  // const [isActiveListPerformer, setActliveListPerformer] = useState(false);
+  // const [title, setTitle] = useState();
+  // const [deadline, setDeadline] = useState('');
+  // const [disabled, setDisabled] = useState(true);
+
+
+
+  const onSubmit = (e) => {
     e.preventDefault();
-    dispatch(createNewTask({ title, deadline, projectId }));
+    dispatch(createNewTask({  projectId }));
     setActive(!active);
   };
 
-  const handleTitleChange = (event) => {
-    event.preventDefault();
-    if (event.target.value !== title) {
-      setDisabled(false);
-    } else {
-      setDisabled(true);
-    }
-    setTitle(event.target.value);
-  };
 
-  const handleDeadlineChange = (event) => {
-    event.preventDefault();
-    if (event.target.value !== deadline) {
-      setDisabled(false);
-    } else {
-      setDisabled(true);
-    }
-    setDeadline(event.target.value);
-  };
 
-  const toggleListPerformer = () => {
-    setActliveListPerformer(!isActiveListPerformer);
-  };
+
+
+  // const onSubmit = (e) => {
+  //   e.preventDefault();
+  //   dispatch(createNewTask({ title, deadline, projectId }));
+  //   setActive(!active);
+  // };
+
+ 
 
   return (
-    <section className={active ? 'createTask__active' : 'createTask'}>
-      <div className='createTask__wrap'>
-        <div className='createTask__info-content'>
-          <input
-            className='createTask__title'
-            value={title || ''}
-            onChange={handleTitleChange}
-            id='title-input'
-            type='text'
-            placeholder='Новая задача'
-            name='title'
-            required
-          />
+    <section className={active ? 'createProject__active' : 'createProject'}>
+      <FormProvider {...methods}> 
+        <form className='createProject__form' onSubmit={methods.handleSubmit(onSubmit)}>
+        <InputField active={active} setActive={setActive} inputPlaceholder={'Новая задача'} isCreateTask={true}/>
+        <div className='createProject__container-btn'>
+          <button className='createProject__submit-btn' type='submit'>+ Создать задачу</button>
         </div>
-        <button
-          className='createTask__cancel-btn'
-          onClick={() => setActive(!active)}>
-        </button>
-      </div>
-
-      <div className='createTask__content'>
-        <form className='createTask__info'>
-          <div className='createTask__container'>
-            <h2 className='createTask__subtitle'>Дата начала</h2>
-            <input
-              className='createTask__input'
-              value={currentDate}
-              id='date-begin-input'
-              type='date'
-              placeholder='Дата начала'
-              name='date_start'
-              required
-            />
-          </div>
-          <div className='createTask__container'>
-            <h2 className='createTask__subtitle'>Дата окончания</h2>
-            <input
-              className='createTask__input'
-              value={deadline}
-              onChange={handleDeadlineChange}
-              id='deadline-input'
-              type='date'
-              placeholder='Дата окончания'
-              name='date_finish'
-              required
-            />
-          </div>
-          <div className='createTask__container-creator'>
-            <h2 className='createTask__subtitle'>Автор</h2>
-            <div className='createTask__creator'>
-              <img
-                src={currentUser.photo !== '' ? avatar : currentUser.photo}
-                className='createTask__creator-avatar'
-                alt='avatar'
-              />
-              <div className='createTask__creator-name'>
-                {currentUser.first_name} {currentUser.last_name}
-              </div>
-            </div>
-          </div>
-          <div className='createTask__container'>
-            <h2 className='createTask__subtitle'>Исполнитель</h2>
-            <div className="createTask__performer" onClick={toggleListPerformer}>
-              <div className="createTask__add-performer"> + Добавить</div>
-              <Select isActiveListPerformer={isActiveListPerformer}
-            setActliveListPerformer={setActliveListPerformer}/>
-            </div>
-          </div>
-          <div className='createTask__container-btn'>
-            <button
-              disabled={disabled}
-              className='createTask__submit-btn'
-              type='submit'
-              onClick={handleAction}
-            >
-              + Создать задачу
-            </button>
-          </div>
         </form>
-      </div>
+      </FormProvider>
     </section>
   );
 }
