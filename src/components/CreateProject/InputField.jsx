ce4e14976ea45f637019189756615e113fc59c3b
BJuice <beetle_juice84@mail.ru>
2023-08-28 22:06:36 +0300

    fix/Стилизовать Task. Создать компонент TaskStatus

--- src/components/CreateProject/InputField.jsx
+++ src/components/CreateProject/InputField.jsx
@@ -97,8 +97,8 @@ function InputField({ active, setActive, inputPlaceholder, isCreateTask }) {
             {...register('title', {
               required: 'Это поле обязательное',
               maxLength: {
-                value: 15,
-                message: `Должно быть не больше 15 символов`,
+                value: isCreateTask ? 50 : 15,
+                message: isCreateTask ? `Должно быть не больше 50 символов` : `Должно быть не больше 15 символов`,
               },
               minLength: {
                 value: 1,
